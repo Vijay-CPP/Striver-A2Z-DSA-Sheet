6d722154c471b6b6c859a70b6b108fd03b0b7a88
T Vijay Kumar <yourstrulyvijay12@gmail.com>
2022-09-30 14:36:53 +0530

    Pattern started

---
+++ Pattern/03_pattern.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    void printTriangle(int n) {
+
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=i; j++)
+                cout<<j<<" ";
+                
+            cout<<"\n";
+        }
+    }
+};
+
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
---
+++ Pattern/07_pattern.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    void printTriangle(int n) {
+        int star = 1;
+        int space = n-1;
+        
+        for(int i=1; i<=n; i++)
+        {
+            // Loop for spaces
+            for(int j=1; j<=space; j++)
+                cout<<" ";
+            
+            // Loop for star
+            for(int j=1; j<=star; j++)
+                cout<<"*";
+            
+            cout<<endl;
+            
+            // Update for next line
+            space--;
+            star += 2;
+        }
+    }
+};
+
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
